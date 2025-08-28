aa12d39a881f3cd77ede3356d6308b51df4fa470
arctic-hen7 <arctic.hen@pm.me>
2025-08-29 07:50:26 +1000

    docs: added lots of docs

--- src/crypto_array.rs
+++ src/crypto_array.rs
@@ -1,11 +1,65 @@
 use std::ops::{Index, IndexMut};
-
 use thiserror::Error;
 use typebits::{
     conditionals::{bitstring::SimpleIf, IsB0},
     And, Array, Bitstring, Diff, Sum, B0,
 };
 
+/// An "array" type for holding cryptographic values.
+///
+/// # How `cryptosystem` handles lengths
+///
+/// ***Tl;DR:* use [`CryptoArraySum`] and [`CryptoArrayDiff`] to create buffers of the right
+/// length, or just use the `encrypt` APIs to work with `Vec<u8>`. If you need to encrypt keys with
+/// other keys, or other fixed-length data, read on!
+///
+/// For symmetric encryption particularly, nearly all algorithms have a fixed-size overhead that
+/// they apply on top of the plaintext. That is, given a plaintext of length `P`, the ciphertext
+/// will be of length `P + O`. This means we can optimise ever so slightly by stack-allocating a
+/// ciphertext `[u8; P + O]` when given a plaintext `[u8; P]`. But if we're given a variable-length
+/// plaintext as, say, a `Vec<u8>`, then we can't do this, so we should return a `Vec<u8>` for the
+/// ciphertext too. The same thing should happen in reverse for the ciphertext. Ideally, we should
+/// also support when a symmetric algorithm doesn't declare its overhead length (perhaps because
+/// it's doing something complex with padding), in which case we should use a `Vec<u8>` for
+/// everything.
+///
+/// This crate achieves this by implementing binary addition in the compiler. No, I'm not kidding,
+/// we built a Turing machine in the damn compiler to make this work. Was it way too much effort
+/// for a tiny optimisation? Yes. Was it fun as all hell? Absolutely!
+///
+/// Specifically, we use [`typebits`] to represent lengths as bitstrings, and then we add them
+/// conditionally so that, if either size is zero (representing variable-length), the result gets
+/// "corrupted" to zero.
+///
+/// Most of the time, end users will use `Vec<u8>`-driven simple APIs, but if they want to, they
+/// can pass in buffers themselves or use the `encrypt_bytes` API, which will return
+/// [`CryptoArray`] buffers.
+///
+/// This type is simple: give it a length as a type parameter, and it will internally work out if
+/// it should be fixed-length (and stack-allocated) ot variable-length (and heap-allocated). The
+/// length parameter needs to be a [`Bitstring`], which you can either construct from binary with
+/// [`typebits::bitstring!`] (don't do this), or you can use [`Const<const N: usize>`], which will
+/// take in an actual number and work from that.
+///
+/// **However**, the vast majority of the time you will never need to do that! We expose
+/// [`CryptoArraySum<A, B>`] and [`CryptoArrayDiff<A, B>`] type aliases that will do the addition
+/// and subtraction for you, given two arrays. For example, if you had a symmetric cryptosystem `K`
+/// and a plaintext 15 bytes long, you could create a ciphertext buffer like so:
+///
+/// ```rust,ignore
+/// let mut buf = CryptoArraySum::<[u8; 15], K::OverheadBytes>::new();
+/// ```
+///
+/// That will internally create a [`CryptoArray`] of exactly the right length, and that length will
+/// adjust at *compile-time* according to the cryptosystem you're using. In other words, you can
+/// write code that's completely generic over the encryption algorithms involved, and still get
+/// everything done on the stack when it's possible. This particularly makes encrypting keys with
+/// other keys (when you know the plaintext length, usually something like 32 bytes) very efficient
+/// and elegant (especially because a fixed-length [`CryptoArray`] serializes to bytes with zero
+/// overhead).
+///
+/// And of course, all this is done at compile-time, and doesn't even need to optimise away, none
+/// of it ends up in your runtime assembly, period.
 pub struct CryptoArray<N: Bitstring>(CryptoArrayInner<N>);
 impl<N: Bitstring> CryptoArray<N> {
     pub fn new() -> Self {
@@ -98,12 +152,12 @@ impl<N: Bitstring> Clone for CryptoArrayInner<N> {
 // consider length 0 it's own thing and we could support `Box<[u8]>` with a generic for
 // size...would be easy to create from `N::UNSIGNED`
 
+/// A trait for values that have a length associated with them, which can be added or subtracted
+/// with each other to produce new cryptographic arrays of the right length. This is implemented
+/// for vectors, and any `[u8; N]` whose `N` we can parse.
 pub trait HasCryptoLen: Clone + AsRef<[u8]> + AsMut<[u8]> {
+    /// The bitstring length of this array, which lets us do fancy addition and subtraction.
     type Length: Bitstring;
-
-    // /// Returns a new instance of this array type, populated with zeroes. For a variable-length
-    // /// array, it will be empty.
-    // fn zeroed() -> Self;
 
     /// Creates a new [`HasCryptoLen`] from the given slice of bytes. This will fail with a
     /// [`BadSize`] error if the given slice is the wrong size (which of course can only be the
@@ -117,6 +171,9 @@ pub trait HasCryptoLen: Clone + AsRef<[u8]> + AsMut<[u8]> {
         self.as_ref().len()
     }
 
+    /// Returns whether or not this is a fixed-length array. If you need this at const time, use
+    /// [`Self::Length::UNSIGNED`] instead, it will be `0` if variable-length, or non-zero if
+    /// fixed.
     fn is_fixed_length() -> bool {
         Self::Length::UNSIGNED != 0
     }
@@ -130,6 +187,7 @@ impl HasCryptoLen for Vec<u8> {
 }
 impl<const N: usize> HasCryptoLen for [u8; N]
 where
+    // Parsing bound!
     Const<N>: ToBitstring,
 {
     type Length = <Const<N> as ToBitstring>::Bitstring;
@@ -154,7 +212,11 @@ impl<B: Bitstring> HasCryptoLen for CryptoArray<B> {
     }
 }
 
+/// A trait for reference types whose owned versions implement [`HasCryptoLen`]. This exists purely
+/// to allow users to pass `&[u8]` where we don't know the length, and for us to interpret these as
+/// variable-length.
 pub trait OwnedHasCryptoLen: AsRef<[u8]> + AsMut<[u8]> {
+    /// The owned type that implements [`HasCryptoLen`].
     type Owned: HasCryptoLen;
 
     /// Gets the *runtime* length of this array. For fixed-length arrays, this will be the same as
@@ -243,54 +305,34 @@ impl<B: Bitstring> CryptoArraySub for B {
 }
 
 /// A type alias for the length of the array created when the two given arrays are summed.
-pub type CryptoArraySumLen<A1 /*: ArrayLen*/, A2 /*: ArrayLen*/> =
+pub type CryptoArraySumLen<A1 /*: HasCryptoLen*/, A2 /*: HasCryptoLen*/> =
     <<A1 as HasCryptoLen>::Length as CryptoArrayAdd>::LenSum<<A2 as HasCryptoLen>::Length>;
 /// A type alias for the length of the array created when the two given arrays are subtracted.
-pub type CryptoArrayDiffLen<A1 /*: ArrayLen*/, A2 /*: ArrayLen*/> =
+pub type CryptoArrayDiffLen<A1 /*: HasCryptoLen*/, A2 /*: HasCryptoLen*/> =
     <<A1 as HasCryptoLen>::Length as CryptoArraySub>::LenDiff<<A2 as HasCryptoLen>::Length>;
 
-/// A type alias for the resulting array created when summing the two given arrays. This will only
-/// work under the following bounds:
-///
-/// ```rust,ignore
-/// where
-///     A1: ArrayLen,
-///     A2: ArrayLen,
-/// ```
-pub type CryptoArraySum<A1 /*: ArrayLen*/, A2 /*: ArrayLen*/> =
+/// A type alias for the resulting array created when summing the two given arrays. You can use
+/// this to create buffers to hold your ciphertexts that will be stack-allocated when they can be,
+/// and variable-length when they need to be, in a way that's generic over the encryption algorithm
+/// used. See [`CryptoArray`] for more details (the output type this will always produce).
+pub type CryptoArraySum<A1 /*: HasCryptoLen*/, A2 /*: HasCryptoLen*/> =
     CryptoArray<CryptoArraySumLen<A1, A2>>;
 
-/// A type alias for the resulting array created when subtracting the two given arrays. This will
-/// only work under the following bounds:
-///
-/// ```rust,ignore
-/// where
-///    A1: ArrayLen,
-///    A2: ArrayLen,
-/// ```
-pub type CryptoArrayDiff<A1 /*: ArrayLen*/, A2 /*: ArrayLen*/> =
+/// A type alias for the resulting array created when subtracting the two given arrays. You can use
+/// this to create buffers to hold your plaintexts that will be stack-allocated when they can be,
+/// and variable-length when they need to be, in a way that's generic over the encryption
+/// algorithm used. See [`CryptoArray`] for more details (the output type this will always
+/// produce).
+pub type CryptoArrayDiff<A1 /*: HasCryptoLen*/, A2 /*: HasCryptoLen*/> =
     CryptoArray<CryptoArrayDiffLen<A1, A2>>;
 
-// /// A type alias for the acceptable length of a buffer to hold the result of a cryptographic
-// /// operation. This is designed to be used in a bound like so:
-// ///
-// /// ```rust, ignore
-// /// where
-// ///     <B as OwnedHasCryptoLen>::Owned:
-// ///         HasCryptoLen<Length = CryptoBufLen<CryptoArraySumLen<P::Owned, C::OverheadBytes>, B>>
-// /// ```
-// ///
-// /// Rather than checking that the buffer `B` has the *exact* length demanded by, say, the sum of
-// /// the plaintext length and the overhead bytes in an encryption operation, `CryptoBufLen` will
-// /// add, and then subtract, the buffer's length from this. If the buffer is of fixed-length, this
-// /// will do nothing, anf that fixed length will be enforced. But if it's variable-length, this will
-// /// corrupt to zero. This essentially allows variable-length buffers to be valid for holding
-// /// fixed-length results.
-// pub type CryptoBufLen<A /*: Bitstring*/, B /*: HasCryptoLen*/> =
-//     <<A as CryptoArrayAdd>::LenSum<<B as HasCryptoLen>::Length> as CryptoArraySub>::LenDiff<
-//         <B as HasCryptoLen>::Length,
-//     >;
-
+/// A marker trait for cryptographic buffers. This doesn't implement any functionality, it's used
+/// as a compile-time way of asserting that a buffer the user provides to an encryption or
+/// decryption function is of the right length.
+///
+/// This is, as you'd expect, implemented for stack-allocated arrays, but `Vec<u8>` implements it
+/// for *any* length, expressing that you can use a variable-length array to store any length of
+/// data.
 pub trait CryptoBuffer<N: Bitstring> {}
 impl<const N: usize> CryptoBuffer<<Const<N> as ToBitstring>::Bitstring> for [u8; N] where
     Const<N>: ToBitstring
@@ -307,12 +349,40 @@ impl<N: Bitstring> CryptoBuffer<N> for CryptoArray<N> {}
 // Once we get specialisation though, go ahead!
 
 /// An intermediate type that has no function other than to implement [`ToBitstring`] for the given
-/// constant.
+/// constant. This acts as a bridge between raw `usize` values and [`Bitstring`].
 pub struct Const<const N: usize>;
 
 /// A trait for converting numbers into bitstrings. This is implemented for a large number of
 /// [`Const<N>`] types.
-pub trait ToBitstring {
+///
+/// # I can't convert to my number!
+///
+/// If you're working with a large or otherwise obscure number (especially if you're doing anything
+/// post-quantum...), a conversion to a bitstring might not be implemented yet! To fix this, you
+/// can either manually construct the bitstring with [`typebits::bitstring!`] (e.g. `bitstring!(1,
+/// 0, 1)` for 5), you can implement [`ToBitstring`] yourself (as below), or you can open a PR
+/// doing this so others can benefit too! Here's how to do the implementation:
+///
+/// ```rust,ignore
+/// use typebits::bs; // Alias for `bitstring!`
+/// use cryptosystem::impl_const;
+///
+/// impl_const!(unsafe 16, bs!(1, 0, 0, 0, 0));
+/// ```
+///
+/// This is an unsafe trait because getting the bitstring wrong will cause panics internally. It
+/// won't lead to memory unsafety, but we mark it as unsafe anyway because it's a very clear and
+/// fundamental design contract, the violation of which will be extremely subtle and very hard to
+/// work out the cause of (imagine the kinds of compiler errors that come from binary addition).
+/// Please double-check your binary!!
+///
+/// Also, note that this is the *only* part of the crate that is not fully generic yet. Once we
+/// have a few nightly features stabilised, we should be able to implement it truly generically. If
+/// anyone has any magical ideas on how to implement it automatically for all `usize` (other than
+/// a build script), I'm open!
+pub unsafe trait ToBitstring {
+    /// The bitstring associated with the given number. The invariant that
+    /// `Self::Bitstring::UNSIGNED == N` must hold.
     type Bitstring: Bitstring;
 }
 
@@ -322,73 +392,3 @@ pub struct BadSize {
     expected: usize,
     found: usize,
 }
-
-// ---
-
-// pub struct True;
-// pub struct False;
-//
-// pub trait TypeBool: ZeroGate {
-//     type And<Other: TypeBool>: TypeBool;
-//     type Or<Other: TypeBool>: TypeBool;
-//     type Not: TypeBool;
-// }
-// impl TypeBool for True {
-//     type And<Other: TypeBool> = Other;
-//     type Or<Other: TypeBool> = True;
-//     type Not = False;
-// }
-// impl TypeBool for False {
-//     type And<Other: TypeBool> = False;
-//     type Or<Other: TypeBool> = Other;
-//     type Not = True;
-// }
-//
-// pub trait IsZero {
-//     type Output: TypeBool;
-// }
-// impl IsZero for UTerm {
-//     type Output = True;
-// }
-// impl<U: Unsigned, B: Bit> IsZero for UInt<U, B> {
-//     type Output = False;
-// }
-//
-// /// A type alias for the associated type of [`IsPositive`]. If `L` or `R` is zero, this will
-// /// evaluate to zero, otherwise it will evaluate to `R`.
-// pub type EitherZero<L, R> = <L as EitherIsZero>::Output<R>;
-// /// When given another [`Unsigned`] type integer, the associated type of this trait will return
-// /// zero if `Self` or the other type is zero, otherwise it will return the other type.
-// pub trait EitherIsZero: IsZero {
-//     type Output<Other: Unsigned + IsZero>: TypeBool + ZeroGate;
-// }
-// impl<U: Unsigned + IsZero> EitherIsZero for U {
-//     type Output<Other: Unsigned + IsZero> =
-//         <<U as IsZero>::Output as TypeBool>::Or<<Other as IsZero>::Output>;
-// }
-//
-// /// A type alias for the associated type of [`ZeroGate`]. If `L` is zero, this will evaluate to
-// /// zero, otherwise it will evaluate to `R`.
-// pub type ZeroGated<L, R> = <L as ZeroGate>::Output<R>;
-//
-// /// When given another [`Unsigned`] type integer, the associated type of this trait will return
-// /// that integer if `Self` is non-zero, otherwise it will return zero.
-// ///
-// /// Said another way, [`ZeroGated<L, R>`] will be zero if `L` is zero, or `R` otherwise.
-// pub trait ZeroGate {
-//     type Output<Other>;
-// }
-// // `ZeroOr<UTerm, X>` should always be zero
-// impl ZeroGate for False {
-//     type Output<Other> = UTerm;
-// }
-// // If our term is non-zero though, take the value of the other term
-// impl ZeroGate for True {
-//     type Output<Other> = Other;
-// }
-//
-// /// A type alias that will evaluate to `X` if neither `A` or `B` is zero, or otherwise zero.
-// pub type EitherZeroOr<A, B, X> = ZeroGated<EitherZero<A, B>, X>;
-//
-// pub trait ArraySize: hybrid_array::ArraySize + Unsigned + EitherIsZero {}
-// impl<U: hybrid_array::ArraySize + Unsigned + IsZero + EitherIsZero> ArraySize for U {}
--- src/sizes.rs
+++ src/sizes.rs
@@ -1,13 +1,16 @@
 use crate::crypto_array::{Const, ToBitstring};
 use typebits::{bs, B0, B1};
 
-impl ToBitstring for Const<0> {
+unsafe impl ToBitstring for Const<0> {
     type Bitstring = B0;
 }
 
+/// A helper macro for implementation [`ToBitstring`] for various constant sizes. You're required
+/// to put the keyword `unsafe` in here
+#[macro_export]
 macro_rules! impl_const {
-    ($const:literal, $bitstring:ty) => {
-        impl $crate::crypto_array::ToBitstring for $crate::crypto_array::Const<$const> {
+    (unsafe $const:literal, $bitstring:ty) => {
+        unsafe impl $crate::crypto_array::ToBitstring for $crate::crypto_array::Const<$const> {
             type Bitstring = $bitstring;
         }
     };
@@ -15,30 +18,30 @@ macro_rules! impl_const {
 
 // REGULAR NUMERIC IMPLEMENTATIONS BEGIN HERE
 
-impl_const!(1, B1);
-impl_const!(2, bs!(1, 0));
-impl_const!(3, bs!(1, 1));
-impl_const!(4, bs!(1, 0, 0));
-impl_const!(5, bs!(1, 0, 1));
-impl_const!(6, bs!(1, 1, 0));
-impl_const!(7, bs!(1, 1, 1));
-impl_const!(8, bs!(1, 0, 0, 0));
-impl_const!(9, bs!(1, 0, 0, 1));
-impl_const!(10, bs!(1, 0, 1, 0));
-impl_const!(11, bs!(1, 0, 1, 1));
-impl_const!(12, bs!(1, 1, 0, 0));
-impl_const!(13, bs!(1, 1, 0, 1));
-impl_const!(14, bs!(1, 1, 1, 0));
-impl_const!(15, bs!(1, 1, 1, 1));
-impl_const!(16, bs!(1, 0, 0, 0, 0));
+impl_const!(unsafe 1, B1);
+impl_const!(unsafe 2, bs!(1, 0));
+impl_const!(unsafe 3, bs!(1, 1));
+impl_const!(unsafe 4, bs!(1, 0, 0));
+impl_const!(unsafe 5, bs!(1, 0, 1));
+impl_const!(unsafe 6, bs!(1, 1, 0));
+impl_const!(unsafe 7, bs!(1, 1, 1));
+impl_const!(unsafe 8, bs!(1, 0, 0, 0));
+impl_const!(unsafe 9, bs!(1, 0, 0, 1));
+impl_const!(unsafe 10, bs!(1, 0, 1, 0));
+impl_const!(unsafe 11, bs!(1, 0, 1, 1));
+impl_const!(unsafe 12, bs!(1, 1, 0, 0));
+impl_const!(unsafe 13, bs!(1, 1, 0, 1));
+impl_const!(unsafe 14, bs!(1, 1, 1, 0));
+impl_const!(unsafe 15, bs!(1, 1, 1, 1));
+impl_const!(unsafe 16, bs!(1, 0, 0, 0, 0));
 
-impl_const!(32, bs!(1, 0, 0, 0, 0, 0));
-impl_const!(40, bs!(1, 0, 1, 0, 0, 0));
-impl_const!(64, bs!(1, 0, 0, 0, 0, 0, 0));
-impl_const!(768, bs!(1, 1, 0, 0, 0, 0, 0, 0, 0, 0));
-impl_const!(800, bs!(1, 1, 0, 0, 1, 0, 0, 0, 0, 0));
-impl_const!(1632, bs!(1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0));
+impl_const!(unsafe 32, bs!(1, 0, 0, 0, 0, 0));
+impl_const!(unsafe 40, bs!(1, 0, 1, 0, 0, 0));
+impl_const!(unsafe 64, bs!(1, 0, 0, 0, 0, 0, 0));
+impl_const!(unsafe 768, bs!(1, 1, 0, 0, 0, 0, 0, 0, 0, 0));
+impl_const!(unsafe 800, bs!(1, 1, 0, 0, 1, 0, 0, 0, 0, 0));
+impl_const!(unsafe 1632, bs!(1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0));
 
-impl_const!(832, bs!(1, 1, 0, 1, 0, 0, 0, 0, 0, 0)); // 32 + 800 for Kyber and x25519 together
-impl_const!(1664, bs!(1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0)); // 32 + 1632 for Kyber and x25519 together
-                                                         // 32 + 768 = 800 for Kyber and x25519, but already covered!
+impl_const!(unsafe 832, bs!(1, 1, 0, 1, 0, 0, 0, 0, 0, 0)); // 32 + 800 for Kyber and x25519 together
+impl_const!(unsafe 1664, bs!(1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0)); // 32 + 1632 for Kyber and x25519 together
+                                                                // 32 + 768 = 800 for Kyber and x25519, but already covered!
