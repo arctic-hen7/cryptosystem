4fcd04088faa8b361a85c101b6d0f82b4356221e
arctic-hen7 <arctic.hen@pm.me>
2025-10-15 20:25:25 +1100

    fix: exported new dilithium cryptosystem

--- src/defaults/mod.rs
+++ src/defaults/mod.rs
@@ -11,6 +11,8 @@ mod x25519;
 
 #[cfg(feature = "default-chacha20poly1305")]
 pub use chacha20_poly1305::*;
+#[cfg(feature = "default-dilithium")]
+pub use dilithium::*;
 #[cfg(feature = "default-ed25519")]
 pub use ed25519::*;
 #[cfg(feature = "default-kyber")]
